1814e8123d522035654b03d53bc8b7a18394f5cb
Merlyn Albery-Speyer <merlyn@newrelic.com>
2014-01-21 19:59:14 -0800

    Add devise user model

--- db/schema.rb
+++ db/schema.rb
@@ -26,10 +26,24 @@ ActiveRecord::Schema.define(version: 20140122180551) do
     t.string   "name"
     t.integer  "event_id"
     t.integer  "shares_outstanding"
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "outcomes", ["event_id"], name: "index_outcomes_on_event_id", using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
