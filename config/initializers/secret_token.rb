b2b6341e6c8e1c15938d61630776f1b31effa884
Merlyn Albery-Speyer <merlyn@newrelic.com>
2014-01-21 17:25:47 -0800

    Generate rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PredictiveMarket::Application.config.secret_key_base = '009b24f6714cc9e0e285219d996c594b46b318a51b5db305cddc528d976a5cf36fdb04791c106a46006700df61f3b79ad96b029136c6cc23d344f59c0c454d8d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PredictiveMarket::Application.config.session_store :cookie_store, key: '_predictive-market_session'
