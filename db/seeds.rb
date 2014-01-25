c1342993abf5ce73aa47a8af3b2b70a3d5640d59
Merlyn Albery-Speyer <merlyn@newrelic.com>
2014-01-25 11:04:04 -0800

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user    = User.create!(email:'foo@bar.com', password:'12345678')
+event   = Event.create!(name: "White Christmas")
+snow    = Outcome.create!(event: event, name: 'Snow at Christmas')
+no_snow = Outcome.create!(event: event, name: 'No snow at Christmas')
+Holding.create!(user: user, outcome: snow, quantity: 10)
