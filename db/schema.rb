d82ccfacb9d6f2c7896b6a89b9233269b23bf8cf
John Hyland <jhyland@newrelic.com>
2014-01-25 19:12:19 -0800

    Add admin field to users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140125214715) do
+ActiveRecord::Schema.define(version: 20140126030229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20140125214715) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.float    "balance",                default: 10000.0, null: false
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
